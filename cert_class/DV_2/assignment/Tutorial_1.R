88ec142c259a36cd23d2ff914afdbbedb0bc3eb9
Roger Andre <randre@gmail.com>
2018-01-30 23:29:23 -0800

    Adding class files from Jan 29

--- cert_class/DV_2/assignment/Tutorial_1.R
+++ cert_class/DV_2/assignment/Tutorial_1.R
@@ -1,7 +1,7 @@
 #install.packages("RODBC")
 #install.packages("data.table")
 #install.packages("sqldf")
-install.packages("tidyverse")
+#install.packages("tidyverse")
 
 require("RODBC")
 
@@ -39,3 +39,34 @@ foo = sqldf(sql)
 glimpse(foo)
 
 foo %>% head(10)
+
+hr_emp = as.data.table(hr_emp)
+
+# USE data.table to make a SQL query, "All EmployeeID with VacationHours > 20
+hr_emp[VacationHours > 20 & Title %like% 'Technician', c("EmployeeID", "LoginID", "VacationHours", "Title")]
+
+as.Date("03-15-2017", "%m-%d-%Y")
+
+dates = fread("date.csv")
+dates
+
+dt = as.data.table(dates)
+
+# Show Total_Sales for Jan 20, 2017
+dt[ Sale_Date == as.Date('2017-01-20', "%Y-%m-%d"), c(Total_Sales)]
+dt[ Sale_Date == as.Date('2017-01-20', "%Y-%m-%d"), ,]
+
+# Show Total_Sales for entire Month of Jan 2017
+dt[ month(Sale_Date) == 1 & year(Sale_Date) == 2017, .( Monthly_Sales = sum(Total_Sales) ), ]
+
+library(lubridate)
+help(ymd)
+dates$Sale_Date <- ymd(dates$Sale_Date)
+glimpse(dates)
+dt = as.data.table(dates)
+glimpse(dt)
+
+# sum(Total_Sales) GROUP BY month(Sale_Date)
+dt[ year(Sale_Date) == 2016, .(Monthly_Sales = sum(Total_Sales)), by = month(Sale_Date) ]
+
+dt[ year(Sale_Date) == 2016, .( Monthly_Sales = sum(Total_Sales) ), ]
